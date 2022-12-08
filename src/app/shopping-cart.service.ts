80e1500f3c81481df6ae4825c88a126a318094b2
vipulaneranjana <vipulana17@gmail.com>
2022-12-08 10:38:14 +0530

    refactorying the addToCart method

--- src/app/shopping-cart.service.ts
+++ src/app/shopping-cart.service.ts
@@ -20,6 +20,10 @@ export class ShoppingCartService {
     return this.db.object('/shopping-carts/' + cartId);
   }
 
+  private getItem(cartId : string, productTitle : string) {
+    return this.db.object('/shopping-carts/' + cartId + '/items/' + productTitle);
+  }
+
   private async getOrCreateCartId(){
     let cartId = localStorage.getItem('cartId');
     if (cartId) return cartId;
@@ -31,10 +35,9 @@ export class ShoppingCartService {
 
   async addToCart(product : Product | any){
     let cartId = await this.getOrCreateCartId();
-    let item$ = this.db.object('/shopping-carts/' + cartId + '/items/' + product.title);
+    let item$ = this.getItem(cartId, product.title);
     item$.snapshotChanges().pipe(take(1)).subscribe(item => {
-      if(item.payload.exists()) item$.update({ quantity : item.payload.exportVal().quantity + 1 });
-      else item$.set({ product: product, quantity: 1})
+    item$.update({ product: product, quantity : (item?.payload?.exportVal()?.quantity | 0) + 1 });
     })
   }
 }

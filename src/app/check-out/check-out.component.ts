321b0aa6b2ab5980258ac20d96ac48257a0fbdc9
vipulaneranjana <vipulana17@gmail.com>
2022-12-16 16:19:35 +0530

    save order in firebase

--- src/app/check-out/check-out.component.ts
+++ src/app/check-out/check-out.component.ts
@@ -1,21 +1,56 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { map, Subscription } from 'rxjs';
+import { ShoppingCart } from '../models/shopping-cart';
+import { OrderService } from '../order.service';
+import { ShoppingCartService } from '../shopping-cart.service';
+
 
 @Component({
   selector: 'app-check-out',
   templateUrl: './check-out.component.html',
   styleUrls: ['./check-out.component.css']
 })
-export class CheckOutComponent implements OnInit {
+export class CheckOutComponent implements OnInit, OnDestroy {
 
   shipping : any = {};
+  cart : ShoppingCart | undefined;
+  subscription : Subscription | undefined;
+  price: number | undefined;
+  items: any;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private orderService : OrderService, private shoppingCartService : ShoppingCartService) { }
+  
+  ngOnDestroy() {
+    this.subscription?.unsubscribe();
   }
 
-  save(shoppingDetail : any ){
-    console.log(shoppingDetail);
+  async ngOnInit(){
+    let cart$ = await this.shoppingCartService.getCart();
+    this.subscription = cart$.valueChanges().subscribe((cart : any) => {
+      this.cart = cart!;
+
+      if (cart) {
+        this.price =0;
+        for (let productTitle of Object.keys(cart!.items)){
+      
+          const quantity = parseFloat(cart!.items[productTitle as keyof typeof cart].quantity);
+          const priceForOne = parseFloat(cart!.items[productTitle as keyof typeof cart].product.price);
+          this.price! += quantity *priceForOne;
+        }
+      }
+    });
+  }
+
+  save(){
+    let order = {
+      datePlaced : new Date().getTime(),
+      shipping : this.shipping,
+      items : this.cart,
+      totalPrice : this.price,
+      
+    };
+    
+    this.orderService.storeOder(order);
   }
 
 }
